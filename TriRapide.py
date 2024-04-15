e5a62f020ce516feafcbf20e969fcb0643a3abbf
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-04-15 09:59:20 +0200

    changing from a classs to a simple fonction

--- TriRapide.py
+++ TriRapide.py
@@ -1,41 +1,34 @@
-import time # Import the time module
+import time
 from list_creator import ListeCreator
 
-class QuickSort:
-    def __init__(self, array):
-        self.array = array
+def partition(array, low, high):
+    i = (low-1)
+    pivot = array[high]
 
-    def partition(self, low, high):
-        i = (low-1)
-        pivot = self.array[high]
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i+1
+            array[i], array[j] = array[j], array[i]
 
-        for j in range(low, high):
-            if self.array[j] <= pivot:
-                i = i+1
-                self.array[i], self.array[j] = self.array[j], self.array[i]
+    array[i+1], array[high] = array[high], array[i+1]
+    return (i+1)
 
-        self.array[i+1], self.array[high] = self.array[high], self.array[i+1]
-        return (i+1)
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi-1)
+        quick_sort(array, pi+1, high)
 
-    def quick_sort(self, low, high):
-        if low < high:
-            pi = self.partition(low, high)
-            self.quick_sort(low, pi-1)
-            self.quick_sort(pi+1, high)
-
-    def sort(self):
-        n = len(self.array)
-        self.quick_sort(0, n-1)
-        return self.array
-
-
+def sort_array(array):
+    n = len(array)
+    quick_sort(array, 0, n-1)
+    return array
 
 if __name__ == "__main__":
-    array = ListeCreator.create_random_array(5000000) # Use the function to create a random array
-    quick_sort = QuickSort(array)
+    array = ListeCreator.create_random_array(5000) # Use the function to create a random array
     
     start_time = time.time() # Capture the start time
-    sorted_array = quick_sort.sort()
+    sorted_array = sort_array(array)
     end_time = time.time() # Capture the end time
 
     print("Array après tri :", sorted_array)
